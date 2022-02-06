a8afab96245530d989555f0975a24dfe4a903104
liuwenhao <liuwenhao@live.com>
2022-02-06 11:41:24 +0800

    feat: 增加组件构建及发布相关配置

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponentElement, useContext, useState } from "react";
 import classNames from "classnames";
 import { MenuContext } from "./Menu";
 import { MenuItemProps } from "./MenuItem";
+import Icon from "../Icon";
 
 
 export interface SubMenuProps {
@@ -16,7 +17,9 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, children, className }) 
     const isOpened = (index && context.mode ==='vertical') ?openedSubMenus.includes(index):false; 
     const [menuOpen, setMenuOpen] = useState(isOpened);
     const classes = classNames('menu-item submenu-item', className, {
-        'is-active': context.index === index
+        'is-active': context.index === index,
+        'is-opened':menuOpen,
+        'is-vertical':context.mode==='vertical'
     })
     const handleClick = (e: React.MouseEvent) => {
         e.preventDefault();
@@ -63,6 +66,7 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, children, className }) 
         <li key={index} className={classes} {...hoverEvent}>
             <div className="submenu-title" {...clickEvent}>
                 {title}
+                <Icon icon="angle-down" className="arrow-icon"/>
             </div>
             {renderChildren()}
         </li>
