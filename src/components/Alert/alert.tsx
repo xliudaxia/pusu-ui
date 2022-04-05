72c46d7c490f7b7de31a285ff15108807dfc3e1c
liuwenhao <liuwenhao@live.com>
2022-04-05 17:55:18 +0800

    feat: 新增alert组件

---
+++ src/components/Alert/alert.tsx
@@ -0,0 +1,49 @@
+import React, { useState, FC } from "react";
+import classNames from "classnames";
+import Icon from "../Icon";
+
+export type AlertType =
+  | "success"
+  | "primary"
+  | "warning"
+  | "danger"
+  | "default";
+export interface AlertProps {
+  title?: string;
+  closeable?: boolean;
+  customClose?: string;
+  onClose?: () => void;
+  type: AlertType;
+}
+
+export const Alert: FC<AlertProps> = (props) => {
+  const { title, closeable, type, customClose, onClose, children } = props;
+  const [visible, setVisible] = useState(true);
+  const classes = classNames("alert", {
+    [`alert-${type}`]: type,
+  });
+  const customCloseP = customClose || (
+    <Icon icon="times" className="window-close" size="lg" />
+  );
+
+  const handleClick = () => {
+    setVisible(false);
+    if (onClose) {
+      onClose();
+    }
+  };
+  return (
+    <div className={classes}>
+      {title ? <h4 className="alert-title">{title}</h4> : null}
+      <p className="alert-message">{children}</p>
+      {closeable ? <i onClick={handleClick}>{customCloseP}</i> : null}
+    </div>
+  );
+};
+
+Alert.defaultProps = {
+  closeable: true,
+  type: "primary",
+};
+
+export default Alert;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
-export { default as Button } from "./components/Button";
-export { default as Menu } from "./components/Menu";
-export { default as Icon } from "./components/Icon";
+// export { default as Button } from "./components/Button";
+// export { default as Menu } from "./components/Menu";
+// export { default as Icon } from "./components/Icon";
 
-export { default as AutoComplete } from "./components/AutoComplete";
-export { default as Input } from "./components/Input";
-export { default as Upload } from "./components/Upload";
-export { default as Progress } from "./components/Progress";
+// export { default as AutoComplete } from "./components/AutoComplete";
+// export { default as Input } from "./components/Input";
+// export { default as Upload } from "./components/Upload";
+// export { default as Progress } from "./components/Progress";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { Upload, UploadFileProps } from "./components/Upload/upload";
+import "./styles/index.scss";
+
+ReactDOM.render(
+  <>
+    <Alert type="success" closeable title="this is Success" />
+    <Alert type="danger" closeable title="this is Danger!" />
+    <Alert type="warning" closeable={false} title="this is Warning!" />
+  </>,
+  document.getElementById("root")
+);
